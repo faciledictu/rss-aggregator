a5e59aea67a324b1f4b9dec23dadf22b20de850a
dmitry zhigulin <jigulin.d@gmail.com>
2022-10-02 14:49:53 +0400

    Configure Webpack

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-### Hexlet tests and linter status:
 [![Actions Status](https://github.com/faciledictu/frontend-project-11/workflows/hexlet-check/badge.svg)](https://github.com/faciledictu/frontend-project-11/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/c6f10a59c9130faefe09/maintainability)](https://codeclimate.com/github/faciledictu/frontend-project-11/maintainability)
+
+## RSS Reader
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      // {
+      //     test: /\.(js|jsx)$/i,
+      //     loader: 'babel-loader',
+      // },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
+};
